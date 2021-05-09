b4df4daf1da5b3b7a8a3c856b774113914de6db0
Aibek <ibookaitkozha@gmail.com>
2021-05-09 23:58:10 +0600

    adding the project skelet

---
+++ Assets/test_script.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("salam");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("lalala");
+    }
+}
